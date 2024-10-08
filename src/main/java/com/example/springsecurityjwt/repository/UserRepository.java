eebd1d37c0099f4d19a410f168d0e6569e47da2c
geunhokinn <gghh1220@gmail.com>
2024-10-08 22:27:18 +0900

    Feat: UserRepository 작성

---
+++ src/main/java/com/example/springsecurityjwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.springsecurityjwt.repository;
+
+import com.example.springsecurityjwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByUsername(String username);
+}
