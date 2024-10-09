f69fa09d72ada2eb03e71923f3be2b846cb361ab
geunhokinn <gghh1220@gmail.com>
2024-10-09 23:21:17 +0900

    Feat: SecurityConfig 작성 - 커스텀 로그인 필터 등록 설정 추가

--- src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
@@ -1,16 +1,31 @@
 package com.example.springsecurityjwt.config;
 
+import com.example.springsecurityjwt.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    // AuthenticationManager 가 인자로 받을 AuthenticationConfiguration 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean // AuthenticationManager Bean 등록
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean // Password encryption
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -35,6 +50,10 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()); // Authorization by path
+
+        // UsernamePasswordAuthenticationFilter 자리를 대체
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         http
                 .sessionManagement((session) -> session
