223a1284d3be147c52fa448547ffda5fe7b99f48
geunhokinn <gghh1220@gmail.com>
2024-10-10 21:03:07 +0900

    Feat: UserRepository 작성 - username 으로 User 를 조회하는 메서드 작성

--- src/main/java/com/example/springsecurityjwt/repository/UserRepository.java
+++ src/main/java/com/example/springsecurityjwt/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.example.springsecurityjwt.repository;
 import com.example.springsecurityjwt.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
