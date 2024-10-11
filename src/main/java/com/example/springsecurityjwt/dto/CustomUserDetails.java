68d85591477b43a86e015f2dfaa6a57e6717dcfa
geunhokinn <gghh1220@gmail.com>
2024-10-11 20:45:13 +0900

    Feat: CustomUserDetails 작성 - loadUSerByUsername() 메서드의 구현체 클래스 작성

---
+++ src/main/java/com/example/springsecurityjwt/dto/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.example.springsecurityjwt.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final InfoDTO infoDTO;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+                return infoDTO.getRole();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return infoDTO.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return infoDTO.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
