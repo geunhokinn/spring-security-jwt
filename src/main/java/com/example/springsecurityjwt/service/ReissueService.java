2b6b01aab87b66c870bcd562e82a664cdffdff19
geunhokinn <gghh1220@gmail.com>
2024-12-07 14:01:08 +0900

    Feat: ReissueService 작성 - 기존에 저장된 RefreshToken 삭제 후 새 RefreshToken 저장하는 로직 추가

--- src/main/java/com/example/springsecurityjwt/service/ReissueService.java
+++ src/main/java/com/example/springsecurityjwt/service/ReissueService.java
@@ -1,6 +1,8 @@
 package com.example.springsecurityjwt.service;
 
+import com.example.springsecurityjwt.entity.RefreshEntity;
 import com.example.springsecurityjwt.jwt.JWTUtil;
+import com.example.springsecurityjwt.repository.RefreshRepository;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,15 +12,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Date;
+
 @Service
 @RequiredArgsConstructor
 public class ReissueService {
 
     private final JWTUtil jwtUtil;
 
+    private final RefreshRepository refreshRepository;
+
     public String reissue(HttpServletRequest request, HttpServletResponse response) {
 
-        //get refresh token
+        // get refresh token
         String refresh = null;
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
@@ -31,16 +37,16 @@ public class ReissueService {
 
         if (refresh == null) {
 
-            //response status code
+            // response status code
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Refresh token is null");
         }
 
-        //expired check
+        // expired check
         try {
             jwtUtil.isExpired(refresh);
         } catch (ExpiredJwtException e) {
 
-            //response status code
+            // response status code
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Refresh token is expired");
         }
 
@@ -53,14 +59,26 @@ public class ReissueService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid refresh token");
         }
 
+        // DB에 저장되어 있는지 확인
+        Boolean isExist = refreshRepository.existsByRefresh(refresh);
+        if (!isExist) {
+
+            // response body
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid refresh token");
+        }
+
         String username = jwtUtil.getUsername(refresh);
         String role = jwtUtil.getRole(refresh);
 
-        //make new JWT
+        // make new JWT
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
         String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
-        //response
+        // Refresh 토큰 저장 DB에 기존의 RefreshToken 삭제 후 새 RefreshToken 저장
+        refreshRepository.deleteByRefresh(refresh);
+        addRefreshEntity(username, newRefresh, 86400000L);
+
+        // response
         response.setHeader("access", newAccess);
         response.addCookie(createCookie("refresh", newRefresh));
 
@@ -77,5 +95,15 @@ public class ReissueService {
         cookie.setHttpOnly(true);
 
         return cookie;
+    }
+
+    // 저장소에 refreshToken 을 저장하는 method
+    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
+        // expireMs 는 만료 시간 -> Date 를 통해 날짜 만들기
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+
+        RefreshEntity refreshEntity = RefreshEntity.buildRefreshEntity(username, refresh, date.toString());
+
+        refreshRepository.save(refreshEntity);
     }
 }
